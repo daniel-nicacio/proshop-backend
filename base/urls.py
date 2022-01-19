8251e71d4835c46bc5d10bd16fa207fd697b7d1a
Daniel Nicácio <70770484+daniel-nicacio@users.noreply.github.com>
2022-01-19 17:33:04 -0300

    User Authentication

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.getRoutes, name="routes"),
-  path('products/', views.getProducts, name="products"),
-  path('products/<str:pk>/', views.getProduct, name="product"),
+    path('users/login/', views.MyTokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('users/register/', views.registerUser, name='register'),
+    path('users/profile/', views.getUserProfile, name="user-profile"),
+    path('users/', views.getUsers, name="user"),
+    path('products/', views.getProducts, name="products"),
+    path('products/<str:pk>/', views.getProduct, name="product"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,46 +1,76 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-from rest_framework import serializers
-
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
 from .models import Product
-from .products import products
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        serializer = UserSerializerWithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail': 'User with this E-mail alredy exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getUserProfile(request):
+    user = request.user
+    serializers = UserSerializer(user, many=False)
+    return Response(serializers.data)
 
 
 @api_view(['GET'])
-def getRoutes(request):
-  routes = [
-    '/api/products/',
-    '/api/products/create',
-
-    '/api/products/update',
-
-    '/api/products/<id>/reviews/',
-
-    '/api/products/top/',
-    '/api/products/<id>/',
-
-    '/api/products/delete/<id>/',
-    '/api/products/<update/<id>/',
-  ]
-  return Response(routes)
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializers = UserSerializer(users, many=True)
+    return Response(serializers.data)
 
 
 @api_view(['GET'])
 def getProducts(request):
-  products = Product.objects.all()
-  serializers = ProductSerializer(products, many=True)
-  return Response(serializers.data)
+    products = Product.objects.all()
+    serializers = ProductSerializer(products, many=True)
+    return Response(serializers.data)
 
 
 @api_view(['GET'])
 def getProduct(request, pk):
-  product = Product.objects.get(_id=pk)
-  serializer = ProductSerializer(product, many=False)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
 
-  return Response(serializer.data)
+    return Response(serializer.data)
