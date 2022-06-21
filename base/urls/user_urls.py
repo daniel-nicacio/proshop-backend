aee299aee9d30ea73cb0ca5eafc4c11c55dd203a
Daniel Nicácio <70770484+daniel-nicacio@users.noreply.github.com>
2022-06-21 02:51:24 -0300

    feat: add endpoint to delete users

--- base/urls/user_urls.py
+++ base/urls/user_urls.py
@@ -4,8 +4,12 @@ from base.views import user_views as views
 urlpatterns = [
     path('login/', views.MyTokenObtainPairView.as_view(),
          name='token_obtain_pair'),
+
     path('register/', views.registerUser, name='register'),
+
     path('profile/', views.getUserProfile, name="user-profile"),
     path('profile/update/', views.updateUserProfile, name="user-profile-update"),
     path('', views.getUsers, name="user"),
+
+    path('delete/<str:pk>/', views.deleteUser, name="user-delete"),
 ]
