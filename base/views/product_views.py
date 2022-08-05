20915d228ee80f66e008a8477f2b7c7c038e949a
Daniel Nicácio <70770484+daniel-nicacio@users.noreply.github.com>
2022-08-04 22:46:56 -0300

    feat: add endpoint to delete product

--- base/views/product_views.py
+++ base/views/product_views.py
@@ -1,8 +1,9 @@
 from base.models import Product
 from base.serializers import ProductSerializer
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser
 
 
 @api_view(['GET'])
@@ -17,3 +18,11 @@ def getProduct(request, pk):
     product = Product.objects.get(_id=pk)
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteProduct(request, pk):
+    product = Product.objects.get(_id=pk)
+    product.delete()
+    return Response('Producted Deleted')
