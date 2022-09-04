353df03d1fb9168397048be8753cd1fd6bdba45f
Daniel Nicácio <70770484+daniel-nicacio@users.noreply.github.com>
2022-09-04 11:15:59 -0300

    feat: add create and update products endpoints

--- base/models.py
+++ base/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, default="/sample.jpg")
     brand = models.CharField(max_length=200, null=True, blank=True)
     category = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
@@ -21,7 +21,7 @@ class Product(models.Model):
     _id = models.AutoField(primary_key=True, editable=False)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
 
 class Review(models.Model):
